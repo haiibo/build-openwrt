2be9e1541fb9c47c2136216412cb1812aa08ef8b
Summer <85640068+haiibo@users.noreply.github.com>
2024-09-01 19:20:47 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -10,10 +10,13 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包+
+rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-mosdns
 # rm -rf feeds/luci/applications/luci-app-netdata
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -61,6 +64,9 @@ git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 # msd_lite
 # git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
 # git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+
+# MosDNS
+git clone --depth=1 -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # Alist
 git_sparse_clone lua https://github.com/sbwml/luci-app-alist luci-app-alist
