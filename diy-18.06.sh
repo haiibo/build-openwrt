4f5c526780f121f416dc9bc7ad5f305028811b3d
haiibo <boyhaibo@gmail.com>
2025-12-18 02:19:21 +0800

    update 2025-12-18 02:19:21

--- diy-18.06.sh
+++ diy-18.06.sh
@@ -284,7 +284,7 @@ if [ $PART_SIZE ]; then
 fi
 
 # 修改默认IP
-[ $DEFAULT_IP ] && sed -i '/n) ipad/s/".*"/"'"$DEFAULT_IP"'"/' package/base-files/files/bin/config_generate
+[ $DEFAULT_IP ] && sed -i '/n) ipad/s/".*"/"'"$DEFAULT_IP"'"/' package/base-files/luci/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
